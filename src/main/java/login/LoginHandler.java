dc59c543fdc3dd6bbd2ed622193d765f2c363fa1
xb <caixubin1010@gmail.com>
2025-03-31 17:44:59 +0800

    make main more OOP by extracting login to a new class login Handler. make user set only strong passwords

--- src/main/java/login/LoginHandler.java
+++ src/main/java/login/LoginHandler.java
@@ -11,43 +11,57 @@ public class LoginHandler {
         this.passwordHolder = passwordHolder;
     }
 
-    public void handleLogin() {
-        boolean run = true;
+    public boolean handleLogin() {
+        int numberOfTries = 5;
 
         if (isDefaultPassword(passwordHolder.getPassWord())) {
             ui.printCreatePasswordMenu();
-            while (run) {
-                String input = ui.getUserCommand();
-                assert input != null : "Error: User input should not be null";
-                if (input.isEmpty()) {
-                    System.out.println("Error: Password requires at least one character");
-                    ui.printDottedLine();
-                } else {
-                    passwordHolder.setPassWord(input);
-                    run = false;
-                    System.out.println("Password created successfully");
-                    ui.printDottedLine();
-                }
-            }
-        } else {
-            while (run) {
-                String input = ui.getUserCommand();
-                assert input != null : "Error: User input should not be null";
 
-                if (passwordHolder.passwordCorrect(input)) {
-                    System.out.println("Login Successful");
-                    ui.printDottedLine();
-                    run = false;
-                } else {
-                    System.out.println("Login Failed: Incorrect password");
-                    ui.printDottedLine();
-                }
+            String input = ui.getUserCommand();
+            assert input != null : "Error: User input should not be null";
+
+            while (!isPasswordStrong(input)) {
+                System.out.println("Error: Password requires at least 8 characters, " +
+                        "using at least 1 uppercase letter, 1 lowercase letter, and 1 number");
+                System.out.println("Please enter new password");
+                ui.printDottedLine();
+                input = ui.getUserCommand();
+            }
+            passwordHolder.setPassWord(input);
+            System.out.println("Password created successfully");
+            ui.printDottedLine();
+            return true;
+        }
+
+        while (numberOfTries > 0) {
+            String input = ui.getUserCommand();
+            assert input != null : "Error: User input should not be null";
+
+            if (passwordHolder.passwordCorrect(input)) {
+                System.out.println("Login Successful");
+                ui.printDottedLine();
+                return true;
+            } else {
+                System.out.println("Login Failed: Incorrect password");
+                System.out.printf("You have %d attempts left\n", numberOfTries);
+                ui.printDottedLine();
+                numberOfTries--;
             }
         }
+        System.out.println("Login Failed, software will be closed");
+        return false;
+    }
+
+    private static boolean isPasswordStrong(String input) {
+        boolean passwordMoreThan8Chars = input.length() >= 8;
+        boolean passwordContainsUppercaseLetters = input.matches(".*[A-Z].*");
+        boolean passwordContainsLowercaseLetters = input.matches(".*[a-z].*");
+        boolean passwordContainsNumbers = input.matches(".*[0-9].*");
+        return passwordContainsNumbers && passwordMoreThan8Chars
+                && passwordContainsUppercaseLetters && passwordContainsLowercaseLetters;
     }
 
     private boolean isDefaultPassword(String password) {
-        return password.equals("12341234 This is a stand in password for your" +
-                " account 12341234 JDNfjndsl jlijfwjfnwjuhun JFBDJBwe7r43rbf jWUEFWUE4RI3B4NKBEifu oiuJWBEFKBLJB");
+        return password.equals("password");
     }
 }
