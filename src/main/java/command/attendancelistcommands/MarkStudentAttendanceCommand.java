d69f63dd0ccfa787a345f96e93bcde2ad63c4c9e
xb <caixubin1010@gmail.com>
2025-04-01 19:12:13 +0800

    fix checkstyle issue

--- src/main/java/command/attendancelistcommands/MarkStudentAttendanceCommand.java
+++ src/main/java/command/attendancelistcommands/MarkStudentAttendanceCommand.java
@@ -43,7 +43,8 @@ public class MarkStudentAttendanceCommand implements Command<AttendanceFile> {
             ArrayList<Student> stdList = theOne.getTutorialClass().getStudentList().getStudents();
             Student derStudent = null;
             for (Student s : stdList) {
-                if (s.getMatricNumber().equalsIgnoreCase(partsArray[3]) && s.getName().equalsIgnoreCase(partsArray[2])) {
+                if (s.getMatricNumber().equalsIgnoreCase(partsArray[3])
+                        && s.getName().equalsIgnoreCase(partsArray[2])) {
                     derStudent = s;
                 }
             }
